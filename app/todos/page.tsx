4b4ae8e331232df0e1e9ae3b41fced0393a1e9b7
k-suke39 <pz.mozarla@gmail.com>
2024-06-26 21:47:42 +0900

    add index & detail page

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,6 +1,6 @@
+
 import prisma from '@/lib/prisma';
-import { addTodo } from '@/lib/actions';
-import DeleteButton from '@/components/delete-button';
+import Link from 'next/link';
 
 const Page = async () => {
   const todos = await prisma.todo.findMany();
@@ -10,22 +10,12 @@ const Page = async () => {
       <h1 className="text-xl font-bold">Todo一覧</h1>
       <ul className="mt-8">
         {todos.map((todo) => (
-        <li key={todo.id} className="flex items-center space-x-2">
+          <li key={todo.id}>
             <span>{todo.name}</span>
-            <DeleteButton id={todo.id} />
-        </li>
+            <Link href={`/todos/${todo.id}`}>詳細</Link>
+          </li>
         ))}
       </ul>
-      <form className="flex items-center mt-4" action={addTodo}>
-        <label htmlFor="name">Name:</label>
-        <input type="text" name="name" className="border mx-2 p-1" />
-        <button
-          type="submit"
-          className="bg-blue-600 px-2 py-1 rounded-lg text-sm text-white"
-        >
-          Add Todo
-        </button>
-      </form>
     </div>
   );
 };
