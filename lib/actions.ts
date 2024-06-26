c902d81956e5b1b27d05d66cb0d590bc7269757f
k-suke39 <pz.mozarla@gmail.com>
2024-06-26 22:04:43 +0900

    add error throw

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,9 +2,15 @@
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
-export const addTodo = async (data: FormData) => {
+export const addTodo = async (prevState: any, data: FormData) => {
   const name = data.get('name') as string;
-  await prisma.todo.create({ data: { name } });
+  try {
+    await prisma.todo.create({ data: { name } });
+  } catch (e) {
+    return {
+      message: 'Failed to add',
+    };
+  }
   revalidatePath('/todos');
   redirect('/todos');
 };
@@ -16,4 +22,20 @@ export const deleteTodo = async (id: number) => {
       },
     });
     revalidatePath('/todos');
+};
+
+export const updateTodo = async (id: number, data: FormData) => {
+  const name = data.get('name') as string;
+  const isCompleted = data.get('isCompleted') as string;
+  await prisma.todo.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      isCompleted: isCompleted === 'true' ? true : false,
+    },
+  });
+  revalidatePath('/todos');
+  redirect('/todos');
 };
